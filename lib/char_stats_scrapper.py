19cd6fe2b217b785c9d4f33256a99a7f7ff90256
Harry Herald <131639717+herald019@users.noreply.github.com>
2024-12-29 01:44:52 +0530

    script for the webscrapper updated
    the web scrapper used to collect character data from keqingmains.com has been updated and the data collected is stored in a new folder for each respective character names.

--- lib/char_stats_scrapper.py
+++ lib/char_stats_scrapper.py
@@ -4,6 +4,8 @@ import requests
 from time import sleep
 from io import StringIO 
 from bs4 import BeautifulSoup
+import os
+from pathlib import Path
 
 #character name and element from genshin.csv from kaggle
 genshin_character_data = pd.read_csv("dataset\genshin.csv", encoding='latin1')
@@ -33,16 +35,33 @@ def char_scrape(element, character):
         html_content = str(tables)
         dfs = pd.read_html(StringIO(html_content))  # Wrap in StringIO
         print(len(dfs))
-        # for i in range(len(dfs)):
-        #     print(f"table {i+1}")
-        #     print(dfs[i], end= "\n\n")
+        return dfs
+        
+
     else:
         print("No tables found on the page.")
+        return 0
+        
 
-# char_scrape("anemo", "wanderer")
+
 
 
 # iterate through all the characters 
 for index, row in req_data.iterrows():
     print(row["character_name"]," : " ,end="")
-    char_scrape(row["vision"], row["character_name"])
+    dfs = char_scrape(row["vision"], row["character_name"])
+
+    directory_name = row["character_name"]
+    directory_path = Path(f"character_tables/{directory_name}")
+
+    try:
+        directory_path.mkdir()
+        print(f"Directory '{directory_path}' created successfully.")
+    except FileExistsError:
+        print(f"Directory '{directory_path}' already exists.")
+    finally:
+        num = 1
+        if dfs != 0 :
+            for df in dfs:
+                df.to_csv(f"{directory_path}/{str(num)}.csv", index=False)
+                num +=1
